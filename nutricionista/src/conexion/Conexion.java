903b3eade1f7cf271ff10f86e5da4fb1d1183560
Luciano Jesus Rodriguez <luciano_rodriguez86@hotmail.com>
2025-02-13 18:23:28 -0300

    commit
    commit

--- nutricionista/src/conexion/Conexion.java
+++ nutricionista/src/conexion/Conexion.java
@@ -6,39 +6,42 @@ import org.mariadb.jdbc.Connection;
 
 public class Conexion {
 
-    private static final String DATABASE_URL = "jdbc:mariadb://";
-    private static final String DATABASE_HOST = "localhost";
-    private static final String DATABASE_PUERTO = ":3306";
-    private static final String DATABASE_DB = "/nutricionista_2024";
-    private static final String DATABASE_USUARIO = "root";
-    private static final String DATABASE_PASSWORD = "";
-    private static Conexion conexion = null;
+    private static final String URL = "jdbc:mariadb://localhost:3306/proyectonutri";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
     private static Connection con = null;
 
     private Conexion() {
         try {
-            // Cargar drivers
+            // Cargar el driver
             Class.forName("org.mariadb.jdbc.Driver");
-
         } catch (ClassNotFoundException ex) {
-            JOptionPane.showMessageDialog(null, "Clase Conexion: Error al cargar Driver" + ex);
+            JOptionPane.showMessageDialog(null, "Error al cargar el Driver: " + ex.getMessage());
         }
     }
 
-    public static Connection getConexion() {
+     public static Connection getConexion() {
         if (con == null) {
             try {
-                con = (Connection) DriverManager.getConnection(
-                        DATABASE_URL + DATABASE_HOST + DATABASE_PUERTO + DATABASE_DB
-                        + "?useLegacyDatetimeCode=false&serverTimezone=UTC",
-                        DATABASE_USUARIO, DATABASE_PASSWORD
-                );
-                System.out.println("Conectado exitosamente");
+                con = (Connection) DriverManager.getConnection(URL, USUARIO, PASSWORD);
+                System.out.println("Conexión establecida correctamente.");
             } catch (SQLException ex) {
                 System.out.println("Error en la conexión: " + ex.getMessage());
             }
         }
         return con;
+    }
+     
+     public static void cerrarConexion() {
+        if (con != null) {
+            try {
+                con.close();
+                con = null;
+                System.out.println("Conexión cerrada.");
+            } catch (SQLException ex) {
+                System.out.println("Error al cerrar la conexión: " + ex.getMessage());
+            }
+        }
     }
 
 
